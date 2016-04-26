91efcff7659b20130b4ef5c0def82021ca15b0fd
ruidongd <ruidongd@outlook.com>
2016-04-25 19:24:56 -0700

    Add an 0 label

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let lable = UILabel()
+        lable.frame = CGRectMake(150, 150, 60, 60)
+        lable.text = "0"
+        
+        // must add the subview to the view so that user can see it
+        self.view.addSubview(lable)
+        
     }
 
     override func didReceiveMemoryWarning() {
